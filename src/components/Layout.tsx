0e11cc6e49cc20bf6f91dd540a929d1b5a51e941
Shaboicarl <thiekematthew@gmail.com>
2025-08-11 18:08:24 -0500

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -695,8 +695,6 @@ export default function Layout({ children }: LayoutProps) {
                   <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
                     guidix.io
                   </span>
-                  <ChevronDown size={16} className="text-gray-400" />
-                </button>
                 </Link>
                 
                 <div className="flex items-center space-x-4">
